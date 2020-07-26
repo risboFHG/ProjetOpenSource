f9b50986ca2d3f19470ba9b29a6e1cd3757df329
risboFHG <tchague.boris.01@gmail.com>
2020-07-26 14:27:03 +0200

    added a python file

---
+++ app.py
@@ -0,0 +1,10 @@
+dictio = {
+        'name' : "Boris TCHAGUE",
+        'age' : 27,
+        'profession' : "Full stack Developpeur - Data Engineer",
+        'address' : "Evry"
+        }
+for key, val in dictio.items :
+    print(key,  " = " + val)
+
+
